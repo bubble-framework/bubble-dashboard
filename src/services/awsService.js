5a022cf5410b2be2f9819d5e5bdf309a82c7573f
Julia Shi <julia.shi@nyu.edu>
2022-07-18 16:09:35 +0100

    awsService for pulling all preview apps for one repo
    Co-authored-by: Andrew Moore <andrewdmoore84@gmail.com>

--- src/services/awsService.js
+++ src/services/awsService.js
@@ -1,3 +1,26 @@
-const repo = 'color-app';
+import wrapExecCmd from '../util/wrapExecCmd';
+
 //pull all preview apps data related to color app
-//
+export const getAllApps = async (repoName) => {
+  try {
+    const apps = await wrapExecCmd(`aws dynamodb scan --table-name ${repoName}-PreviewApps --profile ${repoName}-bubble-user`);
+    const parsed = [];
+    apps.forEach(pullRequest => {
+      if (pullRequest.IsActive.BOOL) {
+        pullRequest.Commits.L.forEach(commit => {
+          const detail = {};
+          detail.pull_request_id = pullRequest.PullRequestId.N;
+          detail.pull_request_name = pullRequest.PRName.S;
+          detail.commit_id = commit.M.CommitId.S.slice(0, 7);
+          detail.commit_message = commit.M.CommitMessageHeader.S;
+          detail.created_at = commit.M.CreatedAt.S;
+          detail.url = 'https://' + commit.M.CloudfrontSubdomain.S + '.cloudfront.net';
+          parsed.push(detail);
+        })
+      }
+    })
+    return parsed;
+  } catch (err) {
+    console.log('The database does not exist for this repo yet; Please run init command first;');
+  };
+};
