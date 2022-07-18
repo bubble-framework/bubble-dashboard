606ad6b37172d073dbe7d04cfac124330257911c
Julia Shi <julia.shi@nyu.edu>
2022-07-18 20:13:07 +0100

    set up backend json server
    Co-authored-by: Andrew Moore <andrewdmoore84@gmail.com>

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 import createError from 'http-errors';
 import express from 'express';
-import awsRouter from './routes/aws.js';
+import dataRouter from './routes/data.js';
 import cors from "cors";
 
 const app = express();
 const PORT = 3005;
 app.use(cors());
 
-app.use('/', awsRouter)
+app.use('/', dataRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- server/routes/data.js
+++ server/routes/data.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { getApps } from '../services/awsService.js';
+import { getAllRepos } from '../services/jsonService.js';
 
 const router = express.Router();
 
 /* GET home page. */
+router.get('/all', function (req, res, next) {
+  const names = getAllRepos();
+  console.log(names);
+  res.json(names);
+})
+
 router.get('/:repoName', async function (req, res, next) {
   const name = req.params.repoName;
   const apps = await getApps(name);
