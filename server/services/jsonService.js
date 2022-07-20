f1afe119a86d2a3a65cc93325dbbecaaff249ebd
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-20 11:21:08 -0500

    Hardcode repo data for ui testing

--- server/services/jsonService.js
+++ server/services/jsonService.js
@@ -2,7 +2,8 @@ import fs from "fs";
 import path from "path";
 
 export const getAllRepos = () => {
-  const activeReposPath = path.join(process.env.HOME, "/.bubble/activeRepos.json");
+  const activeReposPath = path.join(process.env.HOME, "../temp_data_deleteme/activeRepos.json"); // comment out when tailwind is set up
+  // const activeReposPath = path.join(process.env.HOME, "/.bubble/activeRepos.json"); // comment back in when tailwind is set up
   const rawRepoNames = JSON.parse(fs.readFileSync(activeReposPath));
   return rawRepoNames;
 };
