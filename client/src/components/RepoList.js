165bd53a21cc9822d0036b5954a809477a07be73
Missy Lovegren <audrey.lovegren@gmail.com>
2022-08-10 14:09:20 -0500

    Update ui to match our branding

--- client/src/components/RepoList.js
+++ client/src/components/RepoList.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const RepoList = ({ repos }) => (
   <div className="md:flex-col pt-5 flex  justify-between">
-    <p className="py-3 text-xl font-semibold text-indigo-500">
+    <p className="py-3 text-xl font-semibold text-bubble-dark-blue">
       <Link to="/">Repositories</Link>
     </p>
     {repos.map(({ repoName }) => (
       <Link
         key={repoName}
-        className="pt-1"
+        className="pt-1 hover:text-bubble-yellow"
         to={repoName}
       >
         {repoName}
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,12 +1,12 @@
 import RepoList from './RepoList.js';
-import bubbleLogo from '../static/images/noun_project_bubbles_deleteme.png';
+import bubbleLogo from '../static/images/bubble-logo-full-light.png';
 
 const Sidebar = ({ repos }) => {
   return (
     <nav className="w-1/4 relative container mx-auto p-6">
       <div className="flex-col items-center justify-between">
         <div className="pt-2">
-          <img className="w-12 pt-5" src={bubbleLogo} alt="Bubble Logo" />
+          <img className="w-36 pt-5" src={bubbleLogo} alt="Bubble Logo" />
         </div>
         <RepoList repos={repos} />
       </div>
